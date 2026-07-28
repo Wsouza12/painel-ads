61d4360325b686aa9f76ca7029d27cfcccdedf28
Pablo Wanderson <pablowanderson.rj@gmail.com>
2026-07-28 20:39:46 -0300

    feat(analytics): install Google Ads Tag AW-18351203132 in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -35,8 +36,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18351203132"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18351203132');
+          `}
+        </Script>
         {children}
       </body>
     </html>
   );
 }
+
